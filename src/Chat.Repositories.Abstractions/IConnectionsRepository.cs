86b54be8cfa6f4c83f359fdde93d019e1eaf9c7a
pedrohm715 <pedrohm7152@gmail.com>
2019-06-26 12:24:21 -0300

    Implementa padrão de injeção de dependência.

---
+++ src/Chat.Repositories.Abstractions/IConnectionsRepository.cs
@@ -0,0 +1,12 @@
+﻿using Chat.Models;
+using System.Collections.Generic;
+
+namespace Chat.Repositories.Abstractions
+{
+    public interface IConnectionsRepository
+    {
+        void Add(string uniqueID, User user);
+        string GetUserId(long id);
+        List<User> GetAllUser();
+    }
+}
---
+++ src/Chat.Repositories.DependencyInjections/Bootstrapper.cs
@@ -0,0 +1,16 @@
+﻿using Chat.Repositories.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Chat.Repositories.DependencyInjections
+{
+    public static class Bootstrapper
+    {
+        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            services.TryAddSingleton<IConnectionsRepository, ConnectionsRepository>();
+
+            return services;
+        }
+    }
+}
